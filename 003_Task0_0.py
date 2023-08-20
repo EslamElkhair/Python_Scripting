8969e2221834a1ab600dd4b4924b590c997ec619
eslamelkhair <esotech3@gmail.com>
2023-08-20 21:32:03 +0300

    Python program to compute area of a circle

---
+++ 003_Task0_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#****************************************************************************
+
+# python Program to compute the area of a circle 
+
+#****************************************************************************
+
+import math as m
+
+r = float(input("Enter Radius: "))
+
+print("area = ", m.pi * (r**2))  # Bye R^1
